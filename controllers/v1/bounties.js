fd71fe6d6c3bb12931743a2a8addebd6677dc1ba
mtom92 <torresmoranm@gmail.com>
2019-05-06 16:17:05 -0700

    added routes

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -4,7 +4,28 @@ let express = require('express')
 //router instances
 let router = express.Router()
 
-//routes
+//get bounties
 router.get('/', (req,res) => {
   res.send('stub')
 })
+
+//post bounties
+router.post('/', (req,res) => {
+  res.send('stub')
+})
+
+//get bounties/:id
+router.get('/:id', (req,res) => {
+  res.send('stub')
+})
+
+//put bounties
+router.put('/:id', (req,res) => {
+  res.send('stub')
+})
+
+//delete bounties
+router.delete('/:id', (req,res) => {
+  res.send('stub')
+})
+module.exports = router
